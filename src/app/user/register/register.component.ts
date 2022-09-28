5e38ac76637c80ed6593c718b4d309c139860f46
Tola200010 <Tola.cambodia000@gmail.com>
2022-09-28 23:11:33 +0700

    Reusable input and validation

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms'
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class RegisterComponent {
+  name = new FormControl('',[
+    Validators.required
+  ])
+  email = new FormControl('')
+  age = new FormControl('')
+  password = new FormControl('')
+  confirm_password = new FormControl('')
+  phoneNumber = new FormControl('')
+  registerForm = new FormGroup({
+    name : this.name,
+    email: this.email,
+    age : this.age,
+    password: this.password,
+    confirm_password:this.confirm_password,
+    phoneNumber:this.phoneNumber
+  })
 }
